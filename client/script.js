a56cbc77b002869e21df62d9c0d89c443a4e2356
Geoffrey.Noel <126774531+Geoffrey184@users.noreply.github.com>
2023-06-12 11:25:49 +0200

    dva

--- client/script.js
+++ client/script.js
@@ -1,18 +1,51 @@
 // Récupération de la référence vers le bouton et la liste des articles
 const fetchArticlesButton = document.getElementById('fetch-articles-button');
 const articlesList = document.getElementById('articles-list');
+let i = 0;
+const article = document.getElementById('art');
 
 // Gestionnaire d'événement pour le clic sur le bouton
 fetchArticlesButton.addEventListener('click', () => {
-  fetch('/articles') // Envoie une requête GET au serveur
+
+  i++;
+  console.log(i);
+  if (i == 1) {
+  fetch('http://localhost:3000/') // Envoie une requête GET au serveur
     .then(response => response.json()) // Récupère la réponse JSON
     .then(articles => {
-      // Affiche les articles dans la liste
-      articles.forEach(article => {
-        const li = document.createElement('li');
-        li.textContent = article.title;
-        articlesList.appendChild(li);
-      });
-    })
-    .catch(error => console.log(error));
+      console.log(articles)
+     // Crée un conteneur pour les articles
+     const articlesContainer = document.createElement('div');
+     articlesContainer.classList.add('articles-container');
+
+     // Parcourt les articles et affiche-les
+     articles.forEach(article => {
+       const articleElement = document.createElement('div');
+       articleElement.classList.add('article');
+
+       const titleElement = document.createElement('h3');
+       titleElement.textContent = article.title;
+       articleElement.appendChild(titleElement);
+
+       const descriptionElement = document.createElement('p');
+       descriptionElement.textContent = article.description;
+       articleElement.appendChild(descriptionElement);
+
+       const authorElement = document.createElement('p');
+       authorElement.textContent = `Auteur : ${article.author}`;
+       articleElement.appendChild(authorElement);
+
+       articlesContainer.appendChild(articleElement);
+
+       console.log(article)
+     });
+
+     // Efface le contenu précédent de la section des articles
+     
+
+     // Ajoute le conteneur des articles à la section des articles
+     article.appendChild(articlesContainer);
+   })
+   .catch(error => console.log(error));
+  }
 });
--- serveur/server.js
+++ serveur/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const {readFileSync} = require('fs');
+const cors = require('cors');
 const app = express();
 const articles = require('./data.json');
+app.use(cors());
 
 // Configuration pour servir les fichiers statiques
 app.use(express.static('client'));
 
 
 // Endpoint pour récupérer tous les articles
-app.get('/articles', (req, res) => {
-  res.json(articles);
+app.get('/', (req, res) => {
+  res.send(readFileSync('./data.json', 'utf8'));
 });
 
 // Démarrage du serveur
